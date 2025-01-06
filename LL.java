020fa6d989c2263d927f70663f3cef09f18a5cad
RICHA SEN <129830315+richasen1608@users.noreply.github.com>
2025-01-06 18:06:39 +0530

    Add files via upload

---
+++ LL.java
@@ -0,0 +1,43 @@
+public class LL {
+    private Node head;
+    private Node tail;
+
+
+
+    private int size;
+
+    public LL(){
+        this.size=0;
+    }
+    public void insertFirst(int val){
+        Node node=new Node(val);
+        node.next=head;
+        head=node;
+
+        if(tail==null){
+            tail=head;
+        }
+        size+=1;
+    }
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+
+        }
+
+        public Node(Node next) {
+            this.next = next;
+        }
+    }
+
+
+
+
+
+    }
+
+
+

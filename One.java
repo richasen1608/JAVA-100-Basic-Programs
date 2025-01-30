f73226eb09916d3341b37b77fbcf16ba94a450b1
RICHA SEN <129830315+richasen1608@users.noreply.github.com>
2025-01-30 20:40:56 +0530

    Add files via upload

---
+++ One.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class One {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int res;
+        System.out.println("ENTER  a:" );
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.println("ENTER  b:" );
+        b=sc.nextInt();
+        res= a*b;
+        System.out.println("THE RESULT IS "+res);
+
+
+
+    }
+}

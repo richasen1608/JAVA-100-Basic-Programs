c3542fedc33a28cbe7510490840bad51f66f06ac
RICHA SEN <129830315+richasen1608@users.noreply.github.com>
2025-01-31 15:13:06 +0530

    Add files via upload

---
+++ jdbcdEMO.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+import java.sql.*;
+public class jdbcdEMO {
+    public static void main(String[] args) {
+        throw SQLException,ClassNotFoundException{
+            String driverClassName="sun.jdbc.odbc.JdbcOdbcDriver";
+            String url="jdbc.odbc.JdbcOdbcDriver";
+            String username="richa";
+            String password="richa";
+            String query="insert into students values(109,'sam')";
+            Class.forName(driverClassName);
+
+        }
+    }
+}
